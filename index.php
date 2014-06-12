79f4376b6cdb44a8e33a3e047334f625a7190a82
Marko Aleksic <psybaron@gmail.com>
2014-06-12 17:26:24 +0200

    fist commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Marko Z. Aleksić</title>
+        <meta name="description" content="marko aleksic onivo">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="stylesheets/swag.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+            <section id="mainContent">
+                <div id="logo">
+                    <img src="http://lorempixel.com/200/200/" alt="Marko Z. Aleksić Logo">
+                </div>
+                <h1>Marko Z. Aleksić</h1>
+                <p class="lead">
+                    Web Developer <a href="#">@Seavus</a> &middot; Co-Founder of <a href="#">Onivo</a>.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, vitae, at illum id assumenda officia sed dolore voluptatibus? Suscipit, nemo dolorem error ratione consectetur minus saepe dolore asperiores facere a.
+                </p>
+                <hr>
+                <p>
+                    <a href="#">Twitter</a> &middot; <a href="#">LinkedIn</a> &middot; <a href="#">GitHub</a> &middot; <a href="#">Stack Overflow</a>
+                </p>
+            </section>
+            <footer>
+                &copy; 2014 Marko Z. Aleksić
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-43415136-1');ga('send','pageview');
+        </script>
+    </body>
+</html>
