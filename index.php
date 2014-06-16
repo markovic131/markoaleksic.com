71613630e6943e56cf1888768079a99d46ca183c
Marko Aleksic <psybaron@gmail.com>
2014-06-16 10:20:48 +0200

    pre-replease commit

--- index.php
+++ index.php
@@ -1,10 +1,50 @@
+<?php
+    function to_rand_ascii($chr){
+        switch (mt_rand(1, 3)) {
+        case 1:
+            return "&#" . ord($chr) . ";";
+            break;
+        case 2:
+            return "&#000" . ord($chr) . ";";
+            break;
+        default:
+            return "&#x" . dechex(ord($chr)) . ";";
+        }
+    }
+    function mungemail($str_email, $str_display = null, $bln_link = true){
+
+        $str_mailto = '';
+        $str_encoded_email = '';
+
+        for ($i = 0; $i < strlen($str_email); $i++) {
+            $str_encoded_email .= to_rand_ascii(substr($str_email, $i));
+        }
+        if (strlen(trim($str_display)) > 0) {
+            $str_display = $str_display;
+        } else {
+            $str_display = $str_encoded_email;
+        }
+        for ($i = 0; $i < strlen('mailto:'); $i++) {
+            $str_mailto .= to_rand_ascii(substr('mailto:', $i, 1));
+        }
+        return '<a href="mailto:'.$str_display.'">'.$str_display.'</a>';
+    }
+ ?>
 <!doctype html>
+<!--
+ _____         _       _____ _     _       _
+|     |___ ___| |_ ___|  _  | |___| |_ ___|_|___
+| | | | .'|  _| '_| . |     | | -_| '_|_ -| |  _|
+|_|_|_|__,|_| |_,_|___|__|__|_|___|_,_|___|_|___|
+
+ -->
 <html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Marko Z. Aleksić</title>
-        <meta name="description" content="marko aleksic onivo">
+        <meta name="description" content="Marko Z. Aleksić">
+        <meta name="keywords" content="marko, aleksic, aleksić, personal, consulting, business, software, onivo, onivo.mk, invoicing, web-development">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="stylesheets/swag.css">
     </head>
@@ -16,15 +56,21 @@
                 </div> -->
                 <h1>Marko Z. Aleksić</h1>
                 <p class="lead">
-                    Web Developer at <a href="http://seavus.com">Seavus</a> &middot; Co-Founder of <a href="http://onivo.mk">Onivo</a>.
+                    Web Developer at <a href="http://seavus.com" target="_blank">Seavus</a> &middot; Co-Founder of <a href="http://onivo.mk" target="_blank">Onivo</a>.
                 </p>
                 <p>
                     I firmly <strong>belive</strong>; enterprise software meticulous crafted to perfection, can simplify &amp; improve business processes and create competitive advantage.
                     <!-- I kick ass and chew bubble gum. -->
                 </p>
                 <hr>
+                <p id="eml">
+                    Via <?php echo mungemail('iam@markoaleksic.com') ?> or
+                </p>
                 <p>
-                    <a href="#">LinkedIn</a> &middot; <a href="#">Twitter</a> &middot;  <a href="#">GitHub</a> &middot; <a href="#">Stack Overflow</a>
+                    <a href="http://www.linkedin.com/in/aleksicmarko" target="_blank">LinkedIn</a> &middot;
+                    <a href="https://twitter.com/MarkoAleksic131" target="_blank">Twitter</a> &middot;
+                    <a href="https://github.com/psybaron" target="_blank">GitHub</a><!--  &middot;
+                    <a href="http://stackoverflow.com/users/641048/marko-aleksi%C4%87" target="_blank">Stack Overflow</a> -->
                 </p>
             </section>
             <footer>
@@ -33,8 +79,6 @@
                 </p>
             </footer>
         </div>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
         <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
